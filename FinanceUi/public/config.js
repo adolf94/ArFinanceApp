40c024b6c7cb273097ccdc23b68b7caa3aecc9f9
Adolf Rey Along <adolf.rey.along@gmail.com>
2024-09-07 18:03:54 +0800

    updated with envs

--- FinanceUi/public/config.js
+++ FinanceUi/public/config.js
@@ -1,16 +1,6 @@
-window.webConfig = {
-  msal: {
-    auth: {
-      clientId: "7af7001b-6c58-421c-8999-853280293e4b",
-      authority:
-        "https://login.microsoftonline.com/2fa1ddf7-29f1-4d4a-975c-8b29073494af",
-      redirectUri: "/finance",
-      postLogoutRedirectUri: "/logout",
-    },
-    cache: {
-      cacheLocation: "localStorage",
-    },
-    scopes: ["api://7af7001b-6c58-421c-8999-853280293e4b/user_impersonation"],
-  },
-  api: "https://localhost:7129/api",
-};
+window.webConfig =
+{
+        "clientId": "929828408348-sq488sibic3oquur1ov5ke3jos7sgfmv.apps.googleusercontent.com",
+        "redirectUri": "/finance",
+        "api": "https://localhost:7129/api"
+}
