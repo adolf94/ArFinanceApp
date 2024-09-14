4364c429cabf0a013ebde64f2caaad42e47c9ab4
Adolf Rey Along <adolf.rey.along@gmail.com>
2024-09-15 00:11:16 +0800

    remove old files

--- finance-ui-old/public/config/config.js
+++
@@ -1,5 +0,0 @@
-window.webConfig = {
-  api: "https://localhost:7129/api"
-
-
-  }
--- finance-ui-old/src/repositories/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-//@ts-ignore
-const api = axios.create({
-    //@ts-ignore
-    baseURL: window.webConfig.api
-})
-
-
-export default api
