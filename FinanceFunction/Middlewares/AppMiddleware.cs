3a22fb8c65be30c967b4d53425688aeea756ae80
AR Along <adolf.rey.along@gmail.com>
2025-12-09 11:55:13 +0800

    Fixed issue where there is a need to re-auth on refresh.

--- FinanceFunction/Middlewares/AppMiddleware.cs
+++ FinanceFunction/Middlewares/AppMiddleware.cs
@@ -35,7 +35,7 @@ namespace FinanceFunction.Middlewares
 								{
 
 
-										var type = principal.Claims.FirstOrDefault(e => e.Type == "type" && e.Value == "access_token");
+										var type = principal.Claims.FirstOrDefault(e => e.Type == "typ" && e.Value == "access_token");
 										if(type != null)
 										{
 												var userId = principal.Claims.FirstOrDefault(e => e.Type == "userId")?.Value;
